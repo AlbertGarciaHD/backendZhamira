95665bbdf4fc22c901edbabaf471dde141371894
AlbertGarciaHD <84160520+AlbertGarciaHD@users.noreply.github.com>
2025-07-11 12:21:04 -0400

    fix: conexión a la base de datos

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: 'bd_zhamira_user',
-  host: 'd1oiv07diees73cij9hg-a.internal',
+  host: 'd1oiv07diees73cij9hg-a',
   database: 'bd_zhamira',
   password: '0esTpEmP6XrPiFBZQ2lRjVwZEYR78Vmn',
   port: '5432',
@@ -12,3 +12,7 @@ const pool = new Pool({
 });
 //d1oiv07diees73cij9hg-a.oregon-postgres.render.com
 module.exports = pool;
+
+// git add .
+// git commit -m "fix: conexión a la base de datos"
+// git push origin main
