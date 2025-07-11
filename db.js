04dd5500a887bf5d262cef14d040798583a48cb9
AlbertGarciaHD <84160520+AlbertGarciaHD@users.noreply.github.com>
2025-07-11 12:27:38 -0400

    fix: conexión a PostgreSQL corregida

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'bd_zhamira_user',
-  host: 'd1oiv07diees73cij9hg-a',
-  database: 'bd_zhamira',
-  password: '0esTpEmP6XrPiFBZQ2lRjVwZEYR78Vmn',
-  port: '5432',
-  ssl: {
-    rejectUnauthorized: false,
-  }
+  user: "bd_zhamira_user",
+  host: "dpg-d1oiv07diees73cij9hg-a.oregon-postgres.render.com", // <-- Aquí lo completo
+  database: "bd_zhamira",
+  password: "0esTpEmP6XrPiFBZQ2lRjVwZEYR78Vmn",
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
-//d1oiv07diees73cij9hg-a.oregon-postgres.render.com
+
 module.exports = pool;
+
 
 // git add .
 // git commit -m "fix: conexión a la base de datos"
 // git push origin main
+
+//postgresql://bd_zhamira_user:0esTpEmP6XrPiFBZQ2lRjVwZEYR78Vmn@dpg-d1oiv07diees73cij9hg-a/bd_zhamira
