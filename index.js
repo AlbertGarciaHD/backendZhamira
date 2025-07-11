706a4e52882bf85312a002c9ad5b60f8519a7d5c
AlbertGarciaHD <84160520+AlbertGarciaHD@users.noreply.github.com>
2025-07-11 11:51:19 -0400

    add Database

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("¡Servidor activo!");
 });
 
+app.use("/api/users", (res, req) => {
+  res.send("¡Servidor activo!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
