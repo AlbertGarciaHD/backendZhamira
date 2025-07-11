53a54762e4f3943c8c66e2110daae138e4ff8323
AlbertGarciaHD <84160520+AlbertGarciaHD@users.noreply.github.com>
2025-07-11 12:03:39 -0400

    credenciales db 2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: 'bd_zhamira_user',
-  host: 'dpg-d1oiv07diees73cij9hg-a.oregon-postgres.render.com',
+  host: 'dpg-d1oiv07diees73cij9hg-a',
   database: 'bd_zhamira',
   password: '0esTpEmP6XrPiFBZQ2lRjVwZEYR78Vmn',
   port: '5432',
